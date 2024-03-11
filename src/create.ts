fd9558a7502a50ea8baaa54b95f3d9fe67573365
Naimur Rahman <naimur2231@gmail.com>
2024-03-12 00:53:37 +0600

    Creating Multiple Records

--- src/create.ts
+++ src/create.ts
@@ -3,15 +3,36 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
-  // create data
-  const result = await prisma.post.create({
-    data: {
-      title: "first title 3",
-      content: "this is content",
-      authorName: "Naimur Rahman",
-    },
+  //   // create single data
+  //   const result = await prisma.post.create({
+  //     data: {
+  //       title: "first title 3",
+  //       content: "this is content",
+  //       authorName: "Naimur Rahman",
+  //     },
+  //   });
+
+  // create many data
+  const createManny = await prisma.post.createMany({
+    data: [
+      {
+        title: "title 1",
+        content: "this is content 1",
+        authorName: "Author 1",
+      },
+      {
+        title: "title 2",
+        content: "this is content 2",
+        authorName: "Athor 2",
+      },
+      {
+        title: "title 3",
+        content: "this is content 3",
+        authorName: "Author 3",
+      },
+    ],
   });
-  console.log(result);
+  console.log({ createManny });
 };
 
 main();
